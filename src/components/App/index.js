45b058a689dfc45adc95a139b206caecb157d93e
Misha Miroshnikov <41021818+mmiroshnikov@users.noreply.github.com>
2021-11-12 23:08:45 -0800

    Init

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,10 +1,45 @@
-import React from 'react';
-import Messenger from '../Messenger';
+import React, { Fragment, useEffect, useState, useContext } from "react";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link,
+  useRouteMatch,
+  useParams,
+  Redirect
+} from "react-router-dom";
+// import firebase from 'firebase';
+// import { auth, initializeApp, storage } from 'firebase';
+// import styled, { css } from "styled-components";
+import Messenger from "../Messenger";
+// import firebaseSvc from '../FirebaseSvc';
+
 
 export default function App() {
-    return (
-      <div className="App">
-        <Messenger />
+  return (<div className="App">
+
+        <Router>
+          <Routes>
+            <Route
+              exact
+              path="/clear"
+              render={({ history }) => {
+                localStorage.clear();
+                history.push("");
+                return null;
+              }}
+            />
+
+
+            <Route
+              path="/chat"
+              render={(props) => {
+                return <Messenger />;
+              }}
+            />
+
+          </Routes>
+        </Router>
       </div>
-    );
+  );
 }
